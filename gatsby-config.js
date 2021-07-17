96928caae4c99f374c82eb7de605bd4b55557f3e
eabgaryan <eabgaryan@emphasoft.com>
2021-07-17 22:53:45 +0600

    "gatsby-plugin-root-import", "gatsby-plugin-image", "gatsby-plugin-sharp",

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    'gatsby-plugin-root-import',
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
