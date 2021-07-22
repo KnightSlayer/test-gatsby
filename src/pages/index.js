21d8a516891644dab7d7d2e8ceba868b45a4ce7b
eabgaryan <eabgaryan@emphasoft.com>
2021-07-22 10:34:08 +0600

    useState

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,12 @@ import * as React from 'react'
 import Layout from 'src/components/layout'
 
 const IndexPage = () => {
+    const [count, setCount] = React.useState(0);
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby Tutorial.</p>
+
+            <button onClick={() => setCount(c => c + 1)}> I'm clicked {count} times </button>
         </Layout>
     )
 }
